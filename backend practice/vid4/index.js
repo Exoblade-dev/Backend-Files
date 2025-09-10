6435186c0569b72937002195f3088a616f7222bd
Sahil Anand <som.anand10022000@gmail.com>
2025-09-10 20:49:00 +0530

    Update index.js

--- backend practice/vid4/index.js
+++ backend practice/vid4/index.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express()
 
+app.use(function(req,res,next){
+  console.log("middleware chala");
+  next();
+})
+
+app.use(function(req,res,next){
+  console.log("middleware chala ek aur baar");
+  next();
+})
+
 app.get('/',function(req, res){
   res.send("Chala ja bsdk");
+})
+
+app.get('/about',function(req, res){
+  res.send("Chala ja bsdk about page aa gaya ab to");
 })
 
 
