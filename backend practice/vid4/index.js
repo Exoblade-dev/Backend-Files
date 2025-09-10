125921de1175ab92a1a851c74242fa16b32cdd5b
Sahil Anand <som.anand10022000@gmail.com>
2025-09-10 10:56:17 +0530

    Update root route response message
    Changed the response message for the root ('/') route to a new string. No other logic was modified.

--- backend practice/vid4/index.js
+++ backend practice/vid4/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get('/',function(req, res){
+  res.send("Chala ja bsdk");
 })
+
+
 
 app.listen(3000)
